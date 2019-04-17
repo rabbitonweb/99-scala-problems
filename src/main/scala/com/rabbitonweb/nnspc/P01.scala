04c034204cdf56b8cae4469d10be3dbc7b74e5b8
Eugenia Ozirna <eugeunie@gmail.com>
2019-04-17 20:06:40 +0300

    Added tailrec annotation
    Added a tailrec annotation to ensure the method uses a tail call optimization which is one of the main functional programming patterns.

--- src/main/scala/com/rabbitonweb/nnspc/P01.scala
+++ src/main/scala/com/rabbitonweb/nnspc/P01.scala
@@ -1,9 +1,20 @@
 package com.rabbitonweb.nnspc
 
+import scala.annotation.tailrec
+
 class P01[T] {
-  def last(list: List[T]): Option[T] = list match {
-    case Nil => None
-    case head :: Nil => Some(head)
-    case head :: tail => last(tail)
+  def lastElement(list: List[T]): Option[T] = {
+    /**
+     * @tailrec makes sure that the method will be compiled with tail call optimization
+     * that converts the recursive form into a loop
+     */
+    @tailrec
+    def last(list: List[T]): Option[T] = list match {
+      case Nil => None
+      case head :: Nil => Some(head)
+      case head :: tail => last(tail)
+    }
+
+    last(list)
   }
 }
