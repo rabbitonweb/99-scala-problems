9b852ea91c8d339da75b5806a00c24f2c6649991
Eugenia Ozirna <eugeunie@gmail.com>
2019-04-18 14:48:20 +0300

    Added currying and higher-order function
    Added currying and replaced a predicate in if condition with a higher-order function

--- src/main/scala/com/rabbitonweb/nnspc/P03.scala
+++ src/main/scala/com/rabbitonweb/nnspc/P03.scala
@@ -4,11 +4,18 @@ import scala.annotation.tailrec
 
 class P03[T] {
 
-  @tailrec
-  final def nth(n: Int, list: List[T]): Option[T] = (n, list) match {
-    case (0, head :: tail) => Some(head)
-    case (_, Nil) => None
-    case (x, _) if x < 0 => None
-    case (x, head :: tail) => nth(x - 1, tail)
+  final def findNthElement(n: Int, list: List[T])(f: Int => Boolean): Option[T] = {
+    @tailrec
+    def nth(n: Int, list: List[T]): Option[T] = (n, list) match {
+      case (0, head :: tail) => Some(head)
+      case (_, Nil) => None
+      case (x, _) if(f(x)) => None
+      case (x, head :: tail) => nth(x - 1, tail)
+    }
+    nth(n, ls)
   }
+
+  val ls = 1 :: 2 :: 3 :: 3 :: 8 :: 11 :: 22 :: 25 :: 27 :: Nil
+
+  val nthElem = findNthElement(4, ls)(x => x < 0) // returns Some(8)
 }
